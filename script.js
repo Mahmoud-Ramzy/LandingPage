5d2d814d1641f9e47b4aff29c09ec5612dc6956b
Mahmoud Ramzy <anatedo4545@gmail.com>
2026-04-29 19:05:01 +0300

    Landing Page

---
+++ script.js
@@ -0,0 +1,33 @@
+let menu = false;
+async function openNavbar() {
+    if (!menu) {
+        let element = document.getElementById("menu");
+        element.style.display = "flex";
+        element.animate(
+            [
+                // Keyframes
+                { opacity: 0, transform: "translateY(-30px)" }, // Start
+                { opacity: 1, transform: "translateY(0)" }, // End
+            ],
+            {
+                // Timing options
+                duration: 500,
+                fill: "forwards",
+                easing: "ease-out",
+            }
+        );
+        menu = true;
+    } else {
+        let element = document.getElementById("menu");
+        element.animate(
+            [
+                { opacity: 1, transform: "translateY(0)" }, //Start
+                { opacity: 0, transform: "translateY(-30px)" },
+            ],
+            { duration: 500, fill: "forwards", easing: "ease-out" }
+        );
+        await new Promise((r) => setTimeout(r, 500));
+        element.style.display = "none";
+        menu = false;
+    }
+}
